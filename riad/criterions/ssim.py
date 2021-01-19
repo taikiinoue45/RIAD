6317784d7945394711032110e8a801c6ecca7028
TaikiInoue <taikiinoue45@gmail.com>
2021-01-19 14:38:01 +0000

    Initial commit

--- riad/criterions/ssim.py
+++ riad/criterions/ssim.py
@@ -5,53 +5,52 @@ from torch.nn import Module
 
 
 class SSIMLoss(Module):
-    def __init__(self, window_size: int = 11, window_sigma: float = 1.5) -> None:
+    def __init__(self, kernel_size: int = 11, sigma: float = 1.5) -> None:
 
-        self.window_size = window_size
-        self.window_sigma = window_sigma
-        self.window = self._create_gaussian_window(self.windown_size, self.windown_sigma)
+        super().__init__()
+        self.kernel_size = kernel_size
+        self.sigma = sigma
+        self.gaussian_kernel = self._create_gaussian_kernel(self.kernel_size, self.sigma)
 
-    def forward(self, img1: Tensor, img2: Tensor, is_inference: bool = False) -> Tensor:
+    def forward(self, x: Tensor, y: Tensor, is_inference: bool = False) -> Tensor:
 
-        if not self.window.is_cuda():
-            self.window = self.window.to(img1.get_device())
+        if not self.gaussian_kernel.is_cuda:
+            self.kernel = self.gaussian_kernel.to(f"cuda:{x.get_device()}")
 
-        ssim_map = self._ssim(img1, img2)
+        ssim_map = self._ssim(x, y)
 
         if is_inference:
             return ssim_map
         else:
             return ssim_map.mean()
 
-    def _ssim(self, img1: Tensor, img2: Tensor) -> Tensor:
+    def _ssim(self, x: Tensor, y: Tensor) -> Tensor:
 
+        # Compute means
+        ux = F.conv2d(x, self.gaussian_kernel, padding=self.kernel_size // 2, groups=3)
+        uy = F.conv2d(y, self.gaussian_kernel, padding=self.kernel_size // 2, groups=3)
 
-        mu1 = F.conv2d(img1, self.window, padding=self.window_size // 2, groups=channel)
-        mu2 = F.conv2d(img2, self.window, padding=self.window_size // 2, groups=channel)
-        mu1_sq = mu1.pow(2)
-        mu2_sq = mu2.pow(2)
-        mu1_mu2 = mu1 * mu2
-
-        sigma1 = F.conv2d(img1 - mu1, window, padding=window_size // 2, groups=channel)
-        sigma2 = F.conv2d(img2 - mu2, window, padding=window_size // 2, groups=channel)
-        sigma1_sq = sigma1.pow(2)
-        sigma2_sq = sigma2.pow(2)
-        sigma12 = sigma1 * sigma2
+        # Compute variances
+        uxx = F.conv2d(x * x, self.gaussian_kernel, padding=self.kernel_size // 2, groups=3)
+        uyy = F.conv2d(y * y, self.gaussian_kernel, padding=self.kernel_size // 2, groups=3)
+        uxy = F.conv2d(x * y, self.gaussian_kernel, padding=self.kernel_size // 2, groups=3)
+        vx = uxx - ux * ux
+        vy = uyy - uy * uy
+        vxy = uxy - ux * uy
 
         c1 = 0.01 ** 2
         c2 = 0.03 ** 2
-        numerator = (2 * mu1_mu2 + c1) * (2 * sigma12 + c2)
-        denominator = (mu1_sq + mu2_sq + c1) * (sigma1_sq + sigma2_sq + c2)
-        ssim_map = numerator / denominator
-        return ssim_map.mean()
+        numerator = (2 * ux * uy + c1) * (2 * vxy + c2)
+        denominator = (ux ** 2 + uy ** 2 + c1) * (vx + vy + c2)
+        return numerator / denominator
 
-    def _create_gaussian_window(window_size: int, sigma: float) -> Tensor:
+    def _create_gaussian_kernel(self, kernel_size: int, sigma: float) -> Tensor:
 
-        k = torch.arange(kernel_size).to(dtype=torch.float32)
-        k -= (kernel_size - 1) / 2.0
+        start = (1 - kernel_size) / 2
+        end = (1 + kernel_size) / 2
+        kernel_1d = torch.arange(start, end, step=1, dtype=torch.float)
+        kernel_1d = torch.exp(-torch.pow(kernel_1d / sigma, 2) / 2)
+        kernel_1d = (kernel_1d / kernel_1d.sum()).unsqueeze(dim=0)  # (1, kernel_size)
 
-        g = coords ** 2
-        g = (-(g.unsqueeze(0) + g.unsqueeze(1)) / (2 * sigma ** 2)).exp()
-
-        g /= g.sum()
-        return g.unsqueeze(0)
+        kernel_2d = torch.matmul(kernel_1d.t(), kernel_1d)  # (kernel_size, kernel_size)
+        return kernel_2d.expand(3, 1, kernel_size, kernel_size)  # (3, 1, kernel_size, kernel_size)
