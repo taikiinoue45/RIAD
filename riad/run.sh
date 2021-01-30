215730f1dda0fbe725e5c139b8cbde51385d7556
TaikiInoue <taikiinoue45@gmail.com>
2021-01-30 09:35:01 +0900

    Initial commit

--- riad/run.sh
+++ riad/run.sh
@@ -1,16 +1,16 @@
-python run.py configs/dgx.yaml params.category=bottle
-python run.py configs/dgx.yaml params.category=cable
-python run.py configs/dgx.yaml params.category=capsule
-python run.py configs/dgx.yaml params.category=carpet
-python run.py configs/dgx.yaml params.category=grid
-python run.py configs/dgx.yaml params.category=hazelnut
-python run.py configs/dgx.yaml params.category=leather
-python run.py configs/dgx.yaml params.category=metal_nut
-python run.py configs/dgx.yaml params.category=pill
-python run.py configs/dgx.yaml params.category=screw
-python run.py configs/dgx.yaml params.category=tile
-python run.py configs/dgx.yaml params.category=toothbrush
-python run.py configs/dgx.yaml params.category=transistor
-python run.py configs/dgx.yaml params.category=wood
-python run.py configs/dgx.yaml params.category=zipper
+python run.py configs/mvtec.yaml params.cross_validation=0 params.category=bottle
+python run.py configs/mvtec.yaml params.cross_validation=0 params.category=cable
+python run.py configs/mvtec.yaml params.cross_validation=0 params.category=capsule
+python run.py configs/mvtec.yaml params.cross_validation=0 params.category=carpet
+python run.py configs/mvtec.yaml params.cross_validation=0 params.category=grid
+python run.py configs/mvtec.yaml params.cross_validation=0 params.category=hazelnut
+python run.py configs/mvtec.yaml params.cross_validation=0 params.category=leather
+python run.py configs/mvtec.yaml params.cross_validation=0 params.category=metal_nut
+python run.py configs/mvtec.yaml params.cross_validation=0 params.category=pill
+python run.py configs/mvtec.yaml params.cross_validation=0 params.category=screw
+python run.py configs/mvtec.yaml params.cross_validation=0 params.category=tile
+python run.py configs/mvtec.yaml params.cross_validation=0 params.category=toothbrush
+python run.py configs/mvtec.yaml params.cross_validation=0 params.category=transistor
+python run.py configs/mvtec.yaml params.cross_validation=0 params.category=wood
+python run.py configs/mvtec.yaml params.cross_validation=0 params.category=zipper
 
