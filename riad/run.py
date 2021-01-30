28314a3fccbc2354bf802f2d689837d4b48905fc
TaikiInoue <taikiinoue45@gmail.com>
2021-01-30 10:39:25 +0900

    Initial commit

--- riad/run.py
+++ riad/run.py
@@ -18,15 +18,16 @@ sys.argv.pop(1)
 @hydra.main(config_path)
 def main(cfg: DictConfig) -> None:
 
-    mlflow.set_tracking_uri("file:///app/mlruns")
     mlflow.set_experiment(cfg.params.experiment_name)
     mlflow.start_run(run_name=cfg.params.run_name)
     mlflow.log_params(cfg.params)
     mlflow.log_param("cwd", os.getcwd())
-    mlflow.log_artifacts(".hydra", "hydra")
 
     runner = Runner(cfg)
     runner.run()
+
+    mlflow.log_artifacts(".hydra", "hydra")
+    mlflow.log_artifacts("epochs", "epochs")
 
 
 if __name__ == "__main__":
